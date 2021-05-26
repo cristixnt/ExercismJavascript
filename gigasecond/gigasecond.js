8e9e57cfc508b3d9cab4814a67b0d45c42853eb0
Cristian Tejada <ctejadah97@gmail.com>
2021-05-26 09:29:00 -0500

    Ejercicio gigasecond

---
+++ gigasecond/gigasecond.js
@@ -0,0 +1,6 @@
+//
+// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const gigasecond = (d) => new Date(d.getTime() + parseInt(Math.pow(10, 9) + '000'))
